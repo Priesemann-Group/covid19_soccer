5d720ab8ae5341fc756b4cbfa4f3631fe711f7e5
semohr <sebastian.mohr@ds.mpg.de>
2022-05-03 16:22:05 +0000

    Changed plotting font sizes

--- notebooks/header_plotting.py
+++ notebooks/header_plotting.py
@@ -11,12 +11,12 @@ mpl.rcParams["xtick.color"] = "black"
 mpl.rcParams["ytick.color"] = "black"
 mpl.rcParams["axes.spines.right"] = False
 mpl.rcParams["axes.spines.top"] = False
-mpl.rcParams["xtick.labelsize"] = 8
-mpl.rcParams["ytick.labelsize"] = 8
-mpl.rcParams["axes.labelsize"] = 8
-mpl.rcParams["axes.titlesize"] = 8
-mpl.rcParams["figure.titlesize"] = 10
-mpl.rcParams["legend.fontsize"] = 6
+mpl.rcParams["xtick.labelsize"] = 6.5
+mpl.rcParams["ytick.labelsize"] = 6.5
+mpl.rcParams["axes.labelsize"] = 6.5
+mpl.rcParams["axes.titlesize"] = 6.5
+mpl.rcParams["figure.titlesize"] = 6.5
+mpl.rcParams["legend.fontsize"] = 5
 mpl.rcParams["figure.dpi"] = 300  # this primarily affects the size on screen
 
 # Save figure as pdf and png

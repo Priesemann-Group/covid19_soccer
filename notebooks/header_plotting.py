4e9ae42d043e7c0d4284fef0184463abf089176d
semohr <sebastian.mohr@ds.mpg.de>
2022-11-30 16:49:49 +0100

    Adjusted figure size to fit nature requirements and deleted old draft

--- notebooks/header_plotting.py
+++ notebooks/header_plotting.py
@@ -11,13 +11,15 @@ mpl.rcParams["xtick.color"] = "black"
 mpl.rcParams["ytick.color"] = "black"
 mpl.rcParams["axes.spines.right"] = False
 mpl.rcParams["axes.spines.top"] = False
-mpl.rcParams["xtick.labelsize"] = 6.5
-mpl.rcParams["ytick.labelsize"] = 6.5
-mpl.rcParams["axes.labelsize"] = 6.5
-mpl.rcParams["axes.titlesize"] = 6.5
-mpl.rcParams["figure.titlesize"] = 6.5
+mpl.rcParams["xtick.labelsize"] = 6
+mpl.rcParams["ytick.labelsize"] = 6
+mpl.rcParams["axes.labelsize"] = 6
+mpl.rcParams["axes.titlesize"] = 6
+mpl.rcParams["figure.titlesize"] = 6
 mpl.rcParams["legend.fontsize"] = 5
 mpl.rcParams["figure.dpi"] = 300  # this primarily affects the size on screen
 
 # Save figure as pdf and png
 save_kwargs = {"transparent": True, "dpi": 300, "bbox_inches": "tight"}
+COL_WIDTH_1 = 3.46 #88mm
+COL_WIDTH_2 = 7.08 #180mm
